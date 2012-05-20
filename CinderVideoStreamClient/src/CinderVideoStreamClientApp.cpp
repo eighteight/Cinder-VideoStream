11281735026e2c14cdec840ef2abfcbfb23fe955
Vladimir Gusev <bo-ba@mail.ru>
2012-05-20 12:36:52 -0400

    First commit

---
+++ CinderVideoStreamClient/src/CinderVideoStreamClientApp.cpp
@@ -0,0 +1,144 @@
+/*
+ CinderVideoStreamClientApp.cpp
+ 
+ Created by Vladimir Gusev on 5/19/12.
+ Copyright (c) 2012 onewaytheater.us
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of
+ this software and associated documentation files (the "Software"), to deal in
+ the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ of the Software, and to permit persons to whom the Software is furnished to do
+ so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+ */
+
+#include "cinder/app/AppBasic.h"
+#include "cinder/Surface.h"
+#include "cinder/gl/Texture.h"
+#include "cinder/Capture.h"
+#include "cinder/Text.h"
+#include "ConcurrentQueue.h"
+#include "CinderVideoStreamClient.h"
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/lexical_cast.hpp>
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+static const int WIDTH = 320, HEIGHT = 240;
+
+class CinderVideoStreamClientApp : public AppBasic {
+ public:
+    void prepareSettings( Settings *settings );
+	void setup();
+	void keyDown( KeyEvent event );
+	void update();
+	void draw();
+    void shutdown();
+	
+ private:
+
+	gl::Texture	mTexture;
+
+    void threadLoop();
+
+    int cnt;
+    std::shared_ptr<std::thread> mClientThreadRef;
+
+    uint8_t * mData;
+    Surface8u mStreamSurface;
+
+    std::string* mClientStatus;
+    std::string mStatus;
+
+    ph::ConcurrentQueue<uint8_t*>* queueFromServer;
+};
+
+void CinderVideoStreamClientApp::threadLoop()
+{
+    while (true) {
+        try {
+            boost::shared_ptr<CinderVideoStreamClient> s = boost::shared_ptr<CinderVideoStreamClient>(new CinderVideoStreamClient("localhost","3333"));
+            s.get()->setup(queueFromServer, mClientStatus, WIDTH, HEIGHT);
+            s.get()->run();
+        }
+        catch (std::exception& e) {
+            std::cerr << "Exception: " << e.what() << "\n";
+        }
+        //boost::this_thread::sleep(boost::posix_time::milliseconds(1));
+    }
+}
+
+void CinderVideoStreamClientApp::prepareSettings( Settings *settings )
+{
+	settings->setTitle("CinderVideoStreamClient");
+}
+
+void CinderVideoStreamClientApp::setup()
+{	
+    setFrameRate(30);
+    mClientStatus = new std::string();
+    queueFromServer = new ph::ConcurrentQueue<uint8_t*>();
+
+    mClientThreadRef = std::shared_ptr<std::thread>(new boost::thread(boost::bind(&CinderVideoStreamClientApp::threadLoop, this)));
+    
+    mStreamSurface = Surface8u(WIDTH, HEIGHT, 960, SurfaceChannelOrder::RGB);
+}
+
+void CinderVideoStreamClientApp::keyDown( KeyEvent event )
+{
+	if( event.getChar() == 'f' )
+		setFullScreen( ! isFullScreen() );
+}
+
+void CinderVideoStreamClientApp::shutdown(){
+//    if(mClientThreadRef) {
+//        mClientThreadRef->interrupt();
+//        mClientThreadRef->join();
+//    }
+}
+void CinderVideoStreamClientApp::update()
+{
+    if (queueFromServer->try_pop(mData)){
+        memcpy(mStreamSurface.getData(), mData, WIDTH * HEIGHT * 3);
+        mTexture = gl::Texture( mStreamSurface );
+    }
+    mStatus.assign("Client: ").append(boost::lexical_cast<std::string>(getFrameRate())).append(" fps: ").append(*mClientStatus);
+}
+
+void CinderVideoStreamClientApp::draw()
+{
+	gl::enableAlphaBlending();
+	gl::clear( Color::black() );
+
+
+	float width = getWindowWidth();
+	float height = width / ( WIDTH / (float)HEIGHT );
+	float x = 0, y = ( getWindowHeight() - height ) / 2.0f;
+
+	// draw the latest frame
+    gl::color( Color::white() );
+	if( mTexture)
+        gl::draw( mTexture, Rectf( x, y, x + width, y + height ) );
+    
+    // draw status
+    gl::color( Color::black() );	
+    gl::drawString(mStatus, Vec2f( x + 10 + 1, y + 10 + 1 ) );
+}
+
+
+CINDER_APP_BASIC( CinderVideoStreamClientApp, RendererGl )
---
+++ CinderVideoStreamServer/src/CinderVideoStreamServerApp.cpp
@@ -0,0 +1,188 @@
+/*
+ CinderVideoStreamClientApp.cpp
+ 
+ Created by Vladimir Gusev on 5/19/12.
+ Copyright (c) 2012 onewaytheater.us
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of
+ this software and associated documentation files (the "Software"), to deal in
+ the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ of the Software, and to permit persons to whom the Software is furnished to do
+ so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ 
+ Based on CaptureAdvanced Cinder sample
+ */
+#include "cinder/app/AppBasic.h"
+#include "cinder/Surface.h"
+#include "cinder/gl/Texture.h"
+#include "cinder/Capture.h"
+#include "cinder/Text.h"
+#include "ConcurrentQueue.h"
+#include "CinderVideoStreamServer.h"
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/lexical_cast.hpp>
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+static const int WIDTH = 320, HEIGHT = 240;
+
+class CinderVideoStreamServerApp : public AppBasic {
+ public:	
+	void setup();
+	void keyDown( KeyEvent event );
+    void shutdown();
+	void update();
+	void draw();
+	
+ private:
+	vector<Capture>		mCaptures;
+	vector<gl::Texture>	mTextures;
+	vector<gl::Texture>	mNameTextures;
+	vector<Surface>		mRetainedSurfaces;
+    void threadLoop();
+    bool running;
+    std::string mStatus;
+
+    std::shared_ptr<std::thread> mServerThreadRef;
+
+    ph::ConcurrentQueue<uint8_t*>* queueToServer;
+};
+
+void CinderVideoStreamServerApp::threadLoop()
+{
+    while (running) {
+        try {
+            boost::shared_ptr<CinderVideoStreamServer> server = boost::shared_ptr<CinderVideoStreamServer>(new CinderVideoStreamServer(3333,queueToServer, WIDTH, HEIGHT));
+            server.get()->run();
+        }
+        catch (std::exception& e) {
+            std::cerr << "Exception: " << e.what() << "\n";
+        }
+        //boost::this_thread::sleep(boost::posix_time::milliseconds(1));
+    }
+}
+
+void CinderVideoStreamServerApp::setup()
+{
+	// list out the devices
+    setFrameRate(30);
+	vector<Capture::DeviceRef> devices( Capture::getDevices() );
+	for( vector<Capture::DeviceRef>::const_iterator deviceIt = devices.begin(); deviceIt != devices.end(); ++deviceIt ) {
+		Capture::DeviceRef device = *deviceIt;
+		console() << "Found Device " << device->getName() << " ID: " << device->getUniqueId() << std::endl;
+		try {
+			if( device->checkAvailable() ) {
+				mCaptures.push_back( Capture( WIDTH, HEIGHT, device ) );
+				mCaptures.back().start();
+			
+				// placeholder text
+				mTextures.push_back( gl::Texture() );
+
+				// render the name as a texture
+				TextLayout layout;
+				layout.setFont( Font( "Arial", 24 ) );
+				layout.setColor( Color( 1, 1, 1 ) );
+				layout.addLine( device->getName() );
+				mNameTextures.push_back( gl::Texture( layout.render( true ) ) );
+			}
+			else
+				console() << "device is NOT available" << std::endl;
+		}
+		catch( CaptureExc & ) {
+			console() << "Unable to initialize device: " << device->getName() << endl;
+		}
+	}
+    running = true;
+    queueToServer = new ph::ConcurrentQueue<uint8_t*>();
+    mServerThreadRef = std::shared_ptr<std::thread>(new boost::thread(boost::bind(&CinderVideoStreamServerApp::threadLoop, this)));
+}
+
+void CinderVideoStreamServerApp::shutdown(){
+    running = false;
+//    if(mServerThreadRef) {
+//        mServerThreadRef->interrupt();
+//        mServerThreadRef->join();
+//    }
+}
+
+void CinderVideoStreamServerApp::keyDown( KeyEvent event )
+{
+	if( event.getChar() == 'f' )
+		setFullScreen( ! isFullScreen() );
+	else if( event.getChar() == ' ' ) {
+		mCaptures.back().isCapturing() ? mCaptures.back().stop() : mCaptures.back().start();
+	}
+	else if( event.getChar() == 'r' ) {
+		// retain a random surface to exercise the surface caching code
+		int device = rand() % ( mCaptures.size() );
+		mRetainedSurfaces.push_back( mCaptures[device].getSurface() );
+		console() << mRetainedSurfaces.size() << " surfaces retained." << std::endl;
+	}
+	else if( event.getChar() == 'u' ) {
+		// unretain retained surface to exercise the Capture's surface caching code
+		if( ! mRetainedSurfaces.empty() )
+			mRetainedSurfaces.pop_back();
+		console() << mRetainedSurfaces.size() << " surfaces retained." << std::endl;
+	}
+}
+
+void CinderVideoStreamServerApp::update()
+{
+	for( vector<Capture>::iterator cIt = mCaptures.begin(); cIt != mCaptures.end(); ++cIt ) {
+		if( cIt->checkNewFrame() ) {
+			Surface8u surf = cIt->getSurface();
+            queueToServer->push(surf.getData());
+            mTextures[cIt - mCaptures.begin()] = gl::Texture( surf );
+		}
+	}
+    
+    mStatus.assign("Streaming ").append(boost::lexical_cast<std::string>(getFrameRate())).append(" fps");
+}
+
+void CinderVideoStreamServerApp::draw()
+{
+	gl::enableAlphaBlending();
+	gl::clear( Color::black() );
+
+	if( mCaptures.empty() )
+		return;
+
+	float width = getWindowWidth() / mCaptures.size();	
+	float height = width / ( WIDTH / (float)HEIGHT );
+	float x = 0, y = ( getWindowHeight() - height ) / 2.0f;
+	for( vector<Capture>::iterator cIt = mCaptures.begin(); cIt != mCaptures.end(); ++cIt ) {	
+		// draw the latest frame
+		gl::color( Color::white() );
+		if( mTextures[cIt-mCaptures.begin()] )
+			gl::draw( mTextures[cIt-mCaptures.begin()], Rectf( x, y, x + width, y + height ) );
+			
+		// draw the name
+		gl::color( Color::black() );	
+		gl::draw( mNameTextures[cIt-mCaptures.begin()], Vec2f( x + 10 + 1, y + 10 + 1 ) );
+		gl::color( Color( 0.5, 0.75, 1 ) );
+		gl::draw( mNameTextures[cIt-mCaptures.begin()], Vec2f( x + 10, y + 10 ) );
+
+		x += width;
+	}
+    
+    gl::color( Color::black() );	
+    gl::drawString(mStatus, Vec2f(10, getWindowHeight() - 10) );
+}
+
+
+CINDER_APP_BASIC( CinderVideoStreamServerApp, RendererGl )
